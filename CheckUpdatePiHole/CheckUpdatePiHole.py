5863e7ab44be461241a25385a2322405d50d75e2
hubos <fromentin.hugo@gmail.com>
2020-10-20 21:34:47 +0200

    comment

--- CheckUpdatePiHole/CheckUpdatePiHole.py
+++ CheckUpdatePiHole/CheckUpdatePiHole.py
@@ -1,7 +1,10 @@
 import requests
+#get une web page
 site=requests.get("http://pi-hole.local/admin/")
+#if no connection error
 if(site.status_code==200) :
     print("site reachable")
+#if they are an error
 else :
     print("site not reachable")
 print("press enter to quit")
