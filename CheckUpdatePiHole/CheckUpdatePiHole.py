f2e4cfb76d911cde5da210981c9e54c77352489c
hubos <fromentin.hugo@gmail.com>
2020-10-20 22:15:02 +0200

    chaeck update

--- CheckUpdatePiHole/CheckUpdatePiHole.py
+++ CheckUpdatePiHole/CheckUpdatePiHole.py
@@ -1,9 +1,17 @@
 import requests
+import os
+import xml.etree.ElementTree as ElementTree
 #get une web page
 site=requests.get("http://pi-hole.local/admin/")
 #if no connection error
 if(site.status_code==200) :
-    print("site reachable")
+    #get html
+    sitehtml=site.text
+    #search in the html if update is available
+    result=sitehtml.find("Update available!")
+    #if update available write it
+    if(result != -1) :
+        print("Update available!")
 #if they are an error
 else :
     print("site not reachable")
